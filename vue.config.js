a13a4ad20806a21b8d162fd73e6f1ee9711770d9
liangrongzhi <574860113@qq.com>
2021-07-21 22:32:49 +0800

    title

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ module.exports = {
   // 将部署应用程序的基本URL。
   // 默认情况下，Vue CLI假设您的应用程序将部署在域的根目录下。
   // https://www.my-app.com/。如果应用程序部署在子路径上，则需要使用此选项指定子路径。例如，如果您的应用程序部署在https://www.foobar.com/my-app/，集baseUrl到'/my-app/'.
-
   publicPath: process.env.NODE_ENV === 'production' ? './' : './',
 
   // outputDir: 在npm run build时 生成文件的目录 type:string, default:'dist'
@@ -24,7 +23,8 @@ module.exports = {
       // the source template
       template: 'public/index.html',
       // output as dist/index.html
-      filename: 'index.html'
+      filename: 'index.html',
+      title: '五星评价系统'
     }
     // when using the entry-only string format,
     // template is inferred to be `public/subpage.html`
